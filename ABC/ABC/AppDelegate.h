cbe857ceb9ea13ee65b4a053af5810850b5cf0c7
Alaxbb <692667041@qq.com>
2015-07-13 15:06:24 +0800

    New Code <2015.7.13>

---
+++ ABC/ABC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ABC
+//
+//  Created by lanou3g on 15/7/13.
+//  Copyright (c) 2015年 wen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ABC/ABC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ABC
+//
+//  Created by lanou3g on 15/7/13.
+//  Copyright (c) 2015年 wen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
